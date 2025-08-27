92147594669b1fcdc86ca712c39f8fedbb07d84c
ArfaxadRuiz <iron_arfa@hotmail.com>
2025-08-27 09:21:49 -0600

    se crean las vistas protegidas

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import prueba_lista
+from django.contrib.auth import views as auth_views
+from .views import prueba_lista, ProtectedListView
 
 urlpatterns = [
     path('productos/', prueba_lista.as_view(), name='product-list'),
+
+    # vista protegida
+    path("productos-protegidos/", ProtectedListView.as_view(), name="protected_products"),
+
+    # login/logout de Django
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,7 +1,23 @@
 from django.views.generic import ListView
 from .models import ProductModel
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from .models import ProductModel
+from .models import Producto
+
 
 class prueba_lista(ListView):
     model = ProductModel
     template_name = 'e_commerce/product_list.html'
     context_object_name = 'productos'
+
+class ProtectedListView(LoginRequiredMixin, ListView):
+    model = ProductModel
+    template_name = "products/protected_list.html"
+    context_object_name = "products"
+    login_url = "/login/"
+    redirect_field_name = "next"
+
+    def get_queryset(self):
+        # Solo productos del usuario logueado
+        return Producto.objects.filter(user=self.request.user)
